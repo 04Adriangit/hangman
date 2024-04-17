915e077cb949cfdb56dee7902691cfd69b3cf983
04Adriangit <94854278+04Adriangit@users.noreply.github.com>
2024-04-17 13:08:10 +0200

    Create word.h

---
+++ word.h
@@ -0,0 +1,26 @@
+/*
+ * word.h
+ *
+ *  Created on: 17.04.2024
+ *      Author: swt
+ */
+
+#ifndef WORD_H_
+#define WORD_H_
+
+#include <iostream>
+
+std::string words[] = {("ANA"), ("MERE"), ("PERE"), ("REAL"), ("PIANO"),
+		"CALL",
+		"HOROSCOPE",
+		"CREDIBILITY",
+		"GARAGE",
+		"UTTER",
+		"MIGRATION",
+		"CONVICTION",
+		"CASTLE"};
+
+
+
+
+#endif /* WORD_H_ */
